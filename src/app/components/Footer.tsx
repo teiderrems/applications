2ebb34103420751567b5d702f75bd1d54b6389f5
teiderrems <raoul.teida@facsciences-uy1.cm>
2024-05-03 22:55:00 +0200

    update

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,3 +1,4 @@
+import { ContactsOutlined, MailOutlined } from '@ant-design/icons'
 import React from 'react'
 
 export default function Footer() {
@@ -5,7 +6,8 @@ export default function Footer() {
     <footer className='flex flex-col h-1/6 w-full container mx-2 border-t justify-self-end justify-center'>
       
         <h2 className='text-sm text-justify px-2'>&copy; 2024</h2>
-        <h2 className=' text-sm text-justify px-2'>example@yahoo.fr</h2>
+        <h2 className=' text-sm text-justify px-2'><MailOutlined /> example@yahoo.fr</h2>
+        <h2 className=' text-sm text-justify px-2'><ContactsOutlined /> 078563234</h2>
       
     </footer>
   )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ import Link from "next/link";
 import Image from "next/image";
 import burgerImg from '../../public/menu.svg';
 import Head from "next/head";
-import { DownOutlined, HomeOutlined, LogoutOutlined, ProfileOutlined, SmileOutlined, UserOutlined } from "@ant-design/icons";
-import { Dropdown, MenuProps, Space } from "antd";
+import { DownOutlined, HomeOutlined, LogoutOutlined, ProfileOutlined, SmileOutlined, UnorderedListOutlined, UserOutlined } from "@ant-design/icons";
+import { Avatar, Dropdown, MenuProps, Space } from "antd";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -92,7 +92,7 @@ export default function RootLayout({
           }
         }}>
           <div id="toggled-menu"
-              className={`w-full absolute top-0 bottom-0 left-0 -translate-x-full bg-gray-50  ${togel?'translate-x-0 z-20 opacity-100 w-1/5 translate-y-8':''} text-gray-800 border-b border-gray-200 flex flex-col items-center md:static md:w-1/5 md:transform-none md:border-none`}>
+              className={`w-full absolute top-0 bottom-0 left-0 -translate-x-full bg-gradient-to-br from-gray-50 via-slate-50 to-blue-50  ${togel?'translate-x-0 z-20 opacity-100 w-1/5 translate-y-8':''} text-gray-800 border-b border-gray-200 flex flex-col items-center md:static md:w-1/5 md:transform-none md:border-none`}>
             <h1 className="text-xl font-bold p-1 w-full h-12 border-b">Applications</h1>
             <div className="flex-1 flex w-full flex-col"> 
             
@@ -100,9 +100,9 @@ export default function RootLayout({
               user?(
               <nav className="flex flex-col flex-1 py-3 space-y-6 text-justify w-full">
                   <Link href="/" className={`px-2 capitalize text-xl ${pathname=="/"?'active':''}`}><HomeOutlined /> Home</Link>
-                  <Link href="/application" className={`px-2 text-xl ${pathname=="/application"&&'active'}`}>Application</Link>
+                  <Link href="/application" className={`px-2 text-xl ${pathname=="/application"&&'active'}`}><UnorderedListOutlined /> Application</Link>
                   {
-                    user?.role=='admin'&&<Link href="/user" className={`px-2 capitalize text-xl ${pathname=="/user"?'active':''}`}>Users</Link>
+                    user?.role=='admin'&&<Link href="/user" className={`px-2 capitalize text-xl ${pathname=="/user"?'active':''}`}><UnorderedListOutlined /> Users</Link>
                   }
               </nav>
               ):(
@@ -116,24 +116,24 @@ export default function RootLayout({
             <Footer/>
             </div>
           <div className="flex-1 flex flex-col h-screen">
-            <div className="flex flex-row h-12 bg-gray-50  justify-between items-center  w-full border-b">
+            <div className="flex flex-row h-12 bg-gradient-to-bl from-gray-50 via-slate-50 to-blue-50  justify-between items-center  w-full border-b">
               <button
                 aria-label="toggle button"
                 aria-expanded="false"
                 id="menu-btn"
-                className="cursor-pointer w-7 md:hidden"
+                className="cursor-pointer w-8 h-8 md:hidden"
                 onClick={toggleNav}
                 >
                   <Image src={image} alt="button"/>
               </button>
                 <span className=" capitalize pl-1 invisible md:visible md:w-1/3">{getPath()}</span>
                 {
-                  token?<button className="about">
+                  token?<button className="px-1">
                     <Dropdown menu={{ items }}>
                       <a onClick={(e) => e.preventDefault()}>
                         <Space>
-                        <UserOutlined />
-                          Profile
+                        <Avatar icon={<UserOutlined />} />
+                          
                         </Space>
                       </a>
                     </Dropdown>
