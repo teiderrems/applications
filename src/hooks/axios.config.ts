19892d5eb63c18e1715b2a615ff947d97648c895
teiderrems <raoul.teida@facsciences-uy1.cm>
2024-05-11 22:09:26 +0200

    update

--- src/hooks/axios.config.ts
+++ src/hooks/axios.config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 //'http://localhost:5000/api/'
 
 const Axios=axios.create({
-    baseURL:'https://applications-api2-6ahnz1es4-teiderrems-projects.vercel.app/api/'    //'https://applications-api2.vercel.app/api/',
+    baseURL:'https://applications-api2.vercel.app/api/',
 });
 
 export default Axios;
