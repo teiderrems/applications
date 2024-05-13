31a9d5c59159cc13a63985b613cd777e23cb40db
teida <raoul.teida@facsciences-uy1.cm>
2024-05-13 19:58:48 +0200

    update

--- src/app/application/page.tsx
+++ src/app/application/page.tsx
@@ -179,12 +179,6 @@ export default function Application() {
         <table className="w-full text-sm px-2 text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th scope="col" className="p-4">
-                <div className="flex items-center">
-                  <input id="checkbox-all-search" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                  <label htmlFor="checkbox-all-search" className="sr-only">checkbox</label>
-                </div>
-              </th>
               <th scope="col" className="px-4 py-3">
                 Titre
               </th>
@@ -212,15 +206,10 @@ export default function Application() {
             {
               response.data?.map((a: Props) => (
                 <tr key={a._id} className={`border-b dark:bg-gray-800 hover:cursor-pointer dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 ${a.Status == 'reject' ? 'bg-red-300' : 'bg-white'}`}>
-                  <td className="w-4 p-4">
-                    <div className="flex items-center">
-                      <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                      <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
-                    </div>
-                  </td>
-                  <th scope="row" className="px-4 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  
+                  <td scope="row" className="px-4 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     {a.Title}
-                  </th>
+                  </td>
                   <td className="px-4 py-4">
                     {a.Entreprise}
                   </td>
