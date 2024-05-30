6897717b81f6ba54fb0f723fd4a349c3241dc634
teida <raoul.teida@facsciences-uy1.cm>
2024-05-30 19:33:30 +0200

    update

--- src/app/user/profile/page.tsx
+++ src/app/user/profile/page.tsx
@@ -5,9 +5,9 @@ import Axios from "@/hooks/axios.config";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import profileImg from "../../../../public/defaul.jpeg";
-import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import { DeleteOutlined, EditOutlined, LoadingOutlined, SaveOutlined } from "@ant-design/icons";
 import UserDetail from "@/app/components/UserDetail";
-import { Avatar, Button } from "antd";
+import { Avatar, Button, Spin, message } from "antd";
 
 export default function UserDetailInfo() {
   const [param, setParam] = useState<string | undefined>(undefined);
@@ -22,19 +22,89 @@ export default function UserDetailInfo() {
   const [token, setToken] = useState<string>();
   const pathname = usePathname();
   const [reload, setReload] = useState(false);
-  const [edit, setEdit] = useState(false);
-  const [userEdit,setUserEdit]=useState(response.data??{});
-  const [isEditable,setIsEditable]=useState({
-    firstname:false,
-    lastname:false,
-    username:false,
-    email:false
-  })
+  const [submit, setSubmit] = useState(false);
+  const [userEdit, setUserEdit] = useState<any>();
+  const [isEditable, setIsEditable] = useState(false);
   const [profile, setProfile] = useState<any>(undefined);
-  const [handleDelete, setHandleDelete] = useState(false);
-  const HandleDelete = async () => {
+
+  const [messageApi, contextHolder] = message.useMessage();
+    const success = (message:string='profile updated successfully') => {
+        messageApi.open({
+          type: 'success',
+          content: message,
+          duration:2000
+        });
+      };
+    
+      const error = () => {
+        messageApi.open({
+          type: 'error',
+          content: `something wrong try again`,
+        });
+      };
+
+  const HandleClick = async () => {
+    setSubmit(true);
+    setIsEditable(true);
+    setResponse({
+      ...response,
+      isLoading: true,
+      data: null,
+      isError: false,
+      isSuccess: false,
+      error: "",
+      status: 0,
+    });
     try {
-      const res = await Axios.delete("users/" + param, {
+      const res = await Axios.put("users/" + userEdit._id, userEdit, {
+        headers: {
+          Authorization: window.localStorage
+            ? "Bearer " + window.localStorage.getItem("token")
+            : "",
+        },
+      });
+      if (res.status == 201 || res.status == 200) {
+        success();
+        setTimeout(()=>router.refresh(),1500);
+      }
+    } catch (err: any) {
+      if (err.response.status == 401) {
+        try {
+          const res = await Axios.post("users/refresh_token", {
+            refresh: localStorage.getItem("refresh"),
+          });
+          if (res.status == 201 || res.status == 200) {
+            localStorage.setItem("token", res.data.token);
+            localStorage.setItem("refresh", res.data.refresh);
+            if (localStorage.getItem("token")) {
+              setReload(true);
+            }
+          }
+        } catch (err: any) {
+          localStorage.removeItem("token");
+          localStorage.removeItem("refresh");
+          if (err.response.status == 401) {
+            router.push(`/login?ReturnUrl=${pathname}`);
+          }
+          setReload(false);
+        }
+      }
+      error();
+    }
+  };
+
+  const HandleDelete = async () => {
+    setResponse({
+      ...response,
+      isLoading: true,
+      data: null,
+      isError: false,
+      isSuccess: false,
+      error: "",
+      status: 0,
+    });
+    try {
+      const res = await Axios.delete("users/" + userEdit._id, {
         headers: {
           Authorization: window.localStorage
             ? "Bearer " + window.localStorage.getItem("token")
@@ -42,10 +112,18 @@ export default function UserDetailInfo() {
         },
       });
       if (res.status == 204) {
-        localStorage.removeItem("token");
-        localStorage.removeItem("refresh");
-        localStorage.removeItem("userId");
-        router.push("/register");
+        success('profile has been deleted successfully')
+        localStorage.removeItem('token');
+        localStorage.removeItem('refresh');
+        localStorage.removeItem('userId');
+        setTimeout(()=>router.push("/"),1000);
+        setResponse({
+          ...response,
+          isLoading: false,
+          status: res.status,
+          data: res.data,
+          isSuccess: true,
+        });
       }
     } catch (error: any) {
       if (error.response.status == 401) {
@@ -69,6 +147,7 @@ export default function UserDetailInfo() {
           setReload(false);
         }
       }
+      error();
     }
   };
 
@@ -83,7 +162,6 @@ export default function UserDetailInfo() {
         (localStorage.getItem("token") as string) &&
         (localStorage.getItem("token") as string)
     );
-    console.log(localStorage.getItem("userId"));
     const getUser = async () => {
       try {
         const res = await Axios.get(`users/${param}`, {
@@ -97,6 +175,7 @@ export default function UserDetailInfo() {
           if (!res.data.user) {
             router.push("/register");
           }
+          setUserEdit(res.data.user);
           setResponse((state) => {
             return {
               ...state,
@@ -132,8 +211,7 @@ export default function UserDetailInfo() {
         const res = await Axios.get(`profile/${response.data.ProfileId}`);
         const imgb64 = Buffer.from(res.data.image).toString("base64");
         setProfile(
-          (state: string) =>
-            (state = `data:${res.data.minetype};base64,${imgb64}`)
+          (state: string) =>(state = `data:${res.data.minetype};base64,${imgb64}`)
         );
       } catch (error) {
         console.log(error);
@@ -143,7 +221,14 @@ export default function UserDetailInfo() {
       getUser();
       getProfile();
     }
-  }, [param, reload, router, response?.data?.ProfileId, profile, pathname]);
+  }, [
+    param,
+    reload,
+    router,
+    response?.data?.ProfileId,
+    profile,
+    pathname,
+  ]);
 
   if (response?.isLoading || !param) {
     return (
@@ -168,45 +253,87 @@ export default function UserDetailInfo() {
             className="h-[90px] w-[90px] self-start"
             draggable={false}
             size={"large"}
-            src={response?.data?.ProfileId ? profile : profileImg}
+            src={userEdit?.ProfileId ? profile : profileImg}
           />
           <div className=" flex flex-col space-y-2 mt-4">
-            <span className=" font-bold">{response?.data?.Username}</span>
-            <span className="">{response?.data?.Email}</span>
+            <span className=" font-bold">{userEdit?.Username}</span>
+            <span className="">{userEdit?.Email}</span>
           </div>
         </div>
-          <div className="grid grid-cols-2 gap-3 w-[598px]">
-            <input type="text" disabled={!(isEditable.firstname)} onChange={(e)=>setUserEdit((state: any)=>{
-              return {...state,Firstname:e.target.value}
-            })} onDoubleClick={()=>setIsEditable({...isEditable,firstname:!isEditable.firstname})} className="rounded shadow w-[279px] hover:cursor-text py-2 px-[10px]" value={response?.data?.Firstname}/>
-            <input type="text" disabled={!(isEditable.lastname)} onChange={(e)=>setUserEdit((state: any)=>{
-              return {...state,Lastname:e.target.value}
-            })} onDoubleClick={()=>setIsEditable({...isEditable,lastname:!isEditable.lastname})}  className="rounded shadow w-[279px] hover:cursor-text py-2 px-[10px]" value={response?.data?.Lastname}/>
-            <input type="email" disabled={!(isEditable.email)} onChange={(e)=>setUserEdit((state: any)=>{
-              return {...state,Email:e.target.value}
-            })} onDoubleClick={()=>setIsEditable({...isEditable,email:!isEditable.email})}  className="rounded shadow w-[279px] py-2 hover:cursor-text px-[10px]" value={response?.data?.Email}/>
-          </div>
-          <p className="text-wrap w-[598px] overflow-hidden h-[100px]">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ducimus error qui fugit atque porro expedita, blanditiis, minus esse unde facilis. Totam maiores eaque laborum eveniet error quibusdam assumenda tenetur.
-              Culpa quod libero dicta nulla eaque ad? Nisi vitae a aperiam, beatae deleniti magnam ducimus corporis corrupti debitis repudiandae blanditiis tempora esse deserunt ullam nesciunt consequatur amet doloribus alias quis.
-              Inventore maiores placeat molestiae? Laborum cumque error voluptatibus quia expedita recusandae cum, odit beatae quas mollitia distinctio quos architecto possimus, unde ex sint. Asperiores ex magnam autem soluta doloremque pariatur.
-              Obcaecati cum laborum ducimus quo eius minus inventore nobis placeat rem, fugit harum, porro praesentium ea qui neque tempore saepe. Voluptatem odit doloribus necessitatibus tempora repellat ipsa? Hic, sequi distinctio!
-              Corrupti hic repellendus excepturi, accusantium saepe aperiam blanditiis rem dolorem ducimus corporis dolores ex recusandae unde sint. Quae et animi error facilis, totam accusantium fugiat at consequatur cumque. Corrupti, nemo?
-              Hic eos facere vitae incidunt perferendis quos a rerum possimus, ducimus vel? Ex, vitae, facilis corporis voluptatem vel rem ullam dicta, repudiandae eius eos minima eaque quam consequatur provident eveniet!
-          </p>
-      <div className="flex w-[598px] space-x-10">
-        <Button icon={<DeleteOutlined/>} className=" ml-2" danger >Delete</Button>
-        <Button icon={<EditOutlined/>} className=" mr-2" >Edit</Button>
+        <div className="grid grid-cols-2 gap-3 w-[598px]">
+          <input
+            type="text"
+            disabled={!isEditable}
+            onChange={(e) =>
+              setUserEdit({ ...userEdit, Firstname: e.target.value })}
+            className="rounded shadow w-[279px] hover:cursor-text py-2 px-[10px]"
+            value={userEdit?.Firstname}
+          />
+          <input
+            type="text"
+            disabled={!isEditable}
+            onChange={(e) =>
+              setUserEdit({ ...userEdit, Lastname: e.target.value })
+            }
+            className="rounded shadow w-[279px] hover:cursor-text py-2 px-[10px]"
+            value={userEdit?.Lastname}
+          />
+          <input
+            type="email"
+            disabled={!isEditable}
+            onChange={(e) =>
+              setUserEdit({ ...userEdit, Email: e.target.value })
+            }
+            className="rounded shadow w-[279px] py-2 hover:cursor-text px-[10px]"
+            value={userEdit?.Email}
+          />
+        </div>
+        <p className="text-wrap w-[598px] overflow-hidden h-[100px]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+          ducimus error qui fugit atque porro expedita, blanditiis, minus esse
+          unde facilis. Totam maiores eaque laborum eveniet error quibusdam
+          assumenda tenetur. Culpa quod libero dicta nulla eaque ad? Nisi vitae
+          a aperiam, beatae deleniti magnam ducimus corporis corrupti debitis
+          repudiandae blanditiis tempora esse deserunt ullam nesciunt
+          consequatur amet doloribus alias quis. Inventore maiores placeat
+          molestiae? Laborum cumque error voluptatibus quia expedita recusandae
+          cum, odit beatae quas mollitia distinctio quos architecto possimus,
+          unde ex sint. Asperiores ex magnam autem soluta doloremque pariatur.
+          Obcaecati cum laborum ducimus quo eius minus inventore nobis placeat
+          rem, fugit harum, porro praesentium ea qui neque tempore saepe.
+          Voluptatem odit doloribus necessitatibus tempora repellat ipsa? Hic,
+          sequi distinctio! Corrupti hic repellendus excepturi, accusantium
+          saepe aperiam blanditiis rem dolorem ducimus corporis dolores ex
+          recusandae unde sint. Quae et animi error facilis, totam accusantium
+          fugiat at consequatur cumque. Corrupti, nemo? Hic eos facere vitae
+          incidunt perferendis quos a rerum possimus, ducimus vel? Ex, vitae,
+          facilis corporis voluptatem vel rem ullam dicta, repudiandae eius eos
+          minima eaque quam consequatur provident eveniet!
+        </p>
+        <div className="flex w-[598px] space-x-10">
+          <Button
+            icon={<DeleteOutlined />}
+            onClick={HandleDelete}
+            className=" ml-2"
+            danger
+          >
+            Delete
+          </Button>
+          {(!isEditable)?(!submit) ? (
+            <Button icon={<EditOutlined />} onClick={()=>setIsEditable(state=>!state)} className=" mr-2">
+              Edit
+            </Button>
+          ):<Spin  className='md:text-center' indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />} /> : (
+            <Button
+              icon={<SaveOutlined />}
+              className=" mr-2"
+              onClick={HandleClick}
+            >
+              SaveChange
+            </Button>
+          )}
+        </div>
       </div>
-      {edit && (
-        <UserDetail
-          canEdit={false}
-          user={response?.data}
-          setShowDetail={setEdit}
-          setIsAdd={setReload}
-        />
-      )}
-    </div>
     </div>
   );
 }
