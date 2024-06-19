3fbe56c2dc5ec8da9cab1462774b3431b7917cf6
teiderrems <raoul.teida@facsciences-uy1.cm>
2024-06-19 18:46:09 +0200

    add antd layout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,16 +6,22 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { Suspense, useEffect, useState } from "react";
 import { CustomType } from "../components/ApplicationDetail";
 import {
-  EyeInvisibleOutlined,
-  EyeOutlined,
-  FacebookOutlined,
-  GoogleOutlined,
   LoadingOutlined,
-  TwitterOutlined,
 } from "@ant-design/icons";
-import { Button, Divider, Spin, message } from "antd";
-import axios from "axios";
-// import Notify from "../components/Notify";
+import {
+  Button,
+  Card,
+  Form,
+  FormProps,
+  Input,
+  Spin,
+  message,
+} from "antd";
+
+type FieldType = {
+  Username?: string;
+  Password?: string;
+};
 
 function Login() {
   const [user, setUser] = useState<{
@@ -34,43 +40,10 @@ function Login() {
 
   const [messageApi, contextHolder] = message.useMessage();
 
-  const success = () => {
-    messageApi.open({
-      type: "success",
-      content: `Welcome ${user.Username}`,
-      duration: 2000,
-    });
-  };
-
-  const error = (message: any) => {
-    messageApi.open({
-      type: "error",
-      content: message ?? `login failed try again`,
-    });
-  };
-
-  const warning = () => {
-    messageApi.open({
-      type: "warning",
-      content:
-        "Something wrong username or password isn't valid please try again",
-    });
-  };
-
-  const HandleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const onFinish: FormProps<FieldType>["onFinish"] = async(values) => {
     setIsSubmit((state) => !state);
-    setResponse({
-      ...response,
-      status: 0,
-      data: null,
-      isLoading: true,
-      isError: false,
-      error: "",
-      isSuccess: false,
-    });
     try {
-      const res = await Axios.post("users/auth", {
+      const res = await Axios.post("users/login", {
         Username: user.Username,
         Password: user.Password,
       });
@@ -93,12 +66,6 @@ function Login() {
             setIsSubmit((state) => !state);
             router.push("/application");
           }
-          setResponse({
-            ...response,
-            isSuccess: true,
-            isLoading: false,
-            data: res.data,
-          });
         }, 500);
       }
     } catch (er: any) {
@@ -116,122 +83,83 @@ function Login() {
       });
     }
   };
+  const success = () => {
+    messageApi.open({
+      type: "success",
+      content: `Welcome ${user.Username}`,
+      duration: 2000,
+    });
+  };
+
+  const error = (message: any) => {
+    messageApi.open({
+      type: "error",
+      content: message ?? `login failed try again`,
+    });
+  };
+
+  const warning = () => {
+    messageApi.open({
+      type: "warning",
+      content:
+        "Something wrong username or password isn't valid please try again",
+    });
+  };
   const [show, setShow] = useState(false);
   useEffect(() => {
     localStorage.removeItem("token");
   }, [query, isSubmit]);
 
-  const loginFacebook = async () => {
-    try {
-      const res = await axios.get("http://localhost:5000/auth/facebook");
-      console.log(res);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
-    <div className=" h-full flex flex-col text-sm space-y-12 md:space-y-0 md:flex-row">
+    <div
+      className=" h-full flex flex-col justify-center items-center text-sm"
+    >
       {contextHolder}
-      <form
-        action=""
-        onSubmit={HandleSubmit}
-        className=" h-3/5 md:w-4/6  space-y-8 mx-4 flex flex-col justify-center items-center md:h-full"
-      >
-        <h1 className="text-gray-400 w-5/6 h-4 text-justify">
-          Login to continue
-        </h1>
-        <div className="flex w-5/6 flex-col space-y-4">
-          <label htmlFor="Username" className="text-xl">
-            Username
-          </label>
-          <input
-            type="text"
-            id="Username"
-            required
-            onChange={(e) => {
-              setUser({ ...user, Username: e.target.value });
-            }}
-            className="px-2 w-full h-14 shadow rounded-md"
-          />
-        </div>
-        <div className="flex w-5/6 flex-col space-y-4">
-          <label htmlFor="Password" className="text-xl">
-            Password
-          </label>
-          <div className="flex w-full rounded-md h-14 shadow ">
-            <input
-              type={show ? "text" : "password"}
-              id="Password"
-              required
-              min={8}
-              pattern="[a-zA-Z0-9;?,@]{8,15}"
-              onChange={(e) => {
-                setUser({ ...user, Password: e.target.value });
-              }}
-              className="flex-1 pl-2 focus:rounded-l-md"
-            />
-            {show ? (
-              <Button  type="text" onClick={() => setShow(!show)} className="w-1/12 hover:border-0 h-full">
-                <EyeOutlined />
-              </Button>
+      <Card title="Login to continue">
+        <Form
+          name="login"
+          action=""
+          onFinish={onFinish}
+          className=" h-3/5 w-full  space-y-4 mx-1 flex flex-col justify-center items-center md:h-full"
+        >
+          <div className="flex flex-col space-y-1 w-full"
+          >
+            <label htmlFor="Username">Username</label>
+            <Input  placeholder="Input your username or email address" onChange={(e)=>setUser({...user,Username:e.target.value})} name="Username" required/>
+          </div>
+          <div className="flex flex-col space-y-1 w-full"
+          >
+            <label htmlFor="Password">Password</label>
+            <Input.Password placeholder="Input your password" onChange={(e)=>setUser({...user,Password:e.target.value})} name="Password" required min={8} max={15} onError={(e)=>console.log(e)}/>
+          </div>
+          <div
+            className="flex justify-between"
+          >
+            {isSubmit ? (
+              <Spin
+                className="md:text-center"
+                indicator={<LoadingOutlined style={{ fontSize: 16 }} spin />}
+              />
             ) : (
-              <Button type="text"  onClick={() => setShow(!show)} className="w-1/12 hover:border-0 h-full">
-                <EyeInvisibleOutlined />
+              <Button
+                className=" rounded-full"
+                htmlType="submit"
+                type="primary"
+              >
+                Submit
               </Button>
             )}
-          </div>
-        </div>
-        {user.Password != "" &&
-          !user.Password.match("[a-zA-Z0-9;?,@]{8,15}") && (
-            <span className="text-red-400 w-5/6 text-wrap">
-              Password must contain at least 8 characters with a mixture of
-              uppercase, lowercase, numbers
+            <span style={{
+              alignSelf:"end"
+            }} className=" md:self-start md:mt-3 hover:underline">
+              You do not have acount{" "}
+              <Link href="/register" className="text-blue-400">
+                SignUp
+              </Link>
             </span>
-          )}
-        <div className="flex flex-col md:flex-row w-5/6 justify-between h-28 md:h-20">
-          {isSubmit ? (
-            <Spin
-              className="md:text-center"
-              indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />}
-            />
-          ) : (
-            <Button className=" rounded-full" htmlType="submit" type="primary">
-              Submit
-            </Button>
-          )}
-          <Link
-            href="/register"
-            className="text-blue-400 md:self-start md:mt-3 hover:underline"
-          >
-            You do not have acount <strong>SignUp</strong>
-          </Link>
-        </div>
-      </form>
-      <div className=" items-center w-5/6 md: md:w-1/2 justify-center md:mt-10 md:flex-col mx-auto flex self-center space-x-4 md:space-x-0">
-        <Button
-          href="#" icon={<GoogleOutlined className=" text-xl " />}
-          onClick={loginFacebook}
-          className=" flex-col md:flex-row px-2 md:w-56  md:h-12 h-14 md:mb-6 md:rounded-full rounded-xl hover:cursor-pointer  shadow md:justify-between md:space-x-4 hover:bg-blue-500 hover:text-while items-center justify-center flex"
-        >
-          
-          <span className="text-xl  ">Google</span>
-        </Button>
-        <Button icon={<FacebookOutlined className=" text-xl " />}
-          href="#"
-          className=" px-2 py-2  md:w-56 flex-col md:flex-row  md:h-12 h-14 md:mb-6  md:rounded-full rounded-xl hover:cursor-pointer shadow  md:justify-between md:space-x-4 hover:bg-blue-500 hover:text-while items-center justify-center flex"
-        >
-          
-          <span className="text-xl ">Facebook</span>
-        </Button>
-        <Button
-          href="#" icon={<TwitterOutlined className=" text-xl " />}
-          className=" px-2 py-2 md:w-56 flex-col md:flex-row   md:h-12 h-14 md:rounded-full rounded-xl hover:cursor-pointer shadow items-center md:justify-between md:space-x-4 hover:bg-blue-500 hover:text-while justify-center flex"
-        >
-          
-          <span className="text-xl">Twitter</span>
-        </Button>
-      </div>
+          </div>
+        </Form>
+      </Card>
     </div>
   );
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,116 +1,200 @@
-"use client"
+"use client";
 
 import Axios from "@/hooks/axios.config";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import {  useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react";
 import { CustomType } from "../components/ApplicationDetail";
-import { EyeInvisibleOutlined, EyeOutlined, LoadingOutlined } from "@ant-design/icons";
-import { Spin, message } from "antd";
+import {
+  LoadingOutlined,
+  UploadOutlined,
+} from "@ant-design/icons";
+import {
+  Button,
+  Card,
+  Form,
+  Input,
+  Spin,
+  Upload,
+  message,
+} from "antd";
 
 export default function Register() {
+  const [user, setUser] = useState<{
+    Username: string;
+    Password: string;
+    Email: string;
+    ConfirmPassword: string;
+    Profile?: any;
+  }>({
+    Username: "",
+    Password: "",
+    Email: "",
+    ConfirmPassword: "",
+    Profile: undefined,
+  });
+  const router = useRouter();
+  const [response, setResponse] = useState<CustomType>();
+  const [isSubmit, setIsSubmit] = useState(false);
 
-    const [user,setUser]=useState<{
-        Username:string
-        Password:string
-        Profile?:string
-        Email:string
-        ConfirmPassword:string
-    }>({
-        Username:"",
-        Password:"",
-        Email:"",
-        ConfirmPassword:""
+  const HandleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsSubmit(!isSubmit);
+    setResponse({
+      ...response,
+      status: 0,
+      data: null,
+      isLoading: true,
+      isError: false,
+      error: "",
+      isSuccess: false,
     });
-    const router=useRouter();
-    const [response,setResponse]=useState<CustomType>();
-    const [show,setShow]=useState(false);
-    const [showC,setShowC]=useState(false);
-    const inputFileRef = useRef<HTMLInputElement>(null);
-    const [isSubmit,setIsSubmit]=useState(false);
-
-    const HandleSubmit=async(e: React.FormEvent)=>{
-        e.preventDefault();
-        setIsSubmit(!isSubmit);
-        setResponse({...response,status:0,data:null,isLoading:true,isError:false,error:"",isSuccess:false});
-        try {
-            const res=await Axios.post("users",new FormData(document.querySelector('form')!));
-            if (res.status==201 || res.status==200) {
-                success();
-                //setResponse({...response,isSuccess:true,isLoading:false,data:res.data});
-                router.push('/login');
-            }
-        } catch (err:any) {
-            error();
-            setIsSubmit(state=>!state);
-            setResponse({...response,error:"register failed please try again",isError:true,isLoading:false});
-        }
+    try {
+      const res = await Axios.post(
+        "users",
+        new FormData(document.querySelector("form")!)
+      );
+      if (res.status == 201 || res.status == 200) {
+        success();
+        //setResponse({...response,isSuccess:true,isLoading:false,data:res.data});
+        router.push("/login");
+      }
+    } catch (err: any) {
+      error();
+      setIsSubmit((state) => !state);
+      setResponse({
+        ...response,
+        error: "register failed please try again",
+        isError: true,
+        isLoading: false,
+      });
     }
-    useEffect(()=>{
-    },[])
-    const [messageApi, contextHolder] = message.useMessage();
-    const success = () => {
-        messageApi.open({
-          type: 'success',
-          content: `Welcome ${user.Username}`,
-          duration:2000
-        });
-      };
-    
-      const error = () => {
-        messageApi.open({
-          type: 'error',
-          content: `login failed try again`,
-        });
-      };
+  };
+  useEffect(() => {}, []);
+  const [messageApi, contextHolder] = message.useMessage();
+  const success = () => {
+    messageApi.open({
+      type: "success",
+      content: `Welcome ${user.Username}`,
+      duration: 2000,
+    });
+  };
 
+  const error = () => {
+    messageApi.open({
+      type: "error",
+      content: `login failed try again`,
+    });
+  };
+
+  type FieldType = {
+    Username?: string;
+    Password?: string;
+    ConfirmPassword?: string;
+    Email?: string;
+    Profile?: any;
+  };
   return (
-    <div className="min-h-screen flex flex-col mt-2 text-sm justify-center  items-center">
-        {contextHolder}
-        <form action="" onSubmit={HandleSubmit} className="md:w-3/6 w-4/6 md:space-y-3 space-y-10 h-5/6 justify-center items-center flex flex-col" encType="multipart/form-data">
-        <h1 className="text-gray-400 self-start mb-2 text-justify">SignIn to continue</h1>
-        {response?.isError && (<span className="text-red-400 text-center w-full block">{response?.error}</span>)}
-            <div className="h-20 w-full md:my-0 flex space-x-2 flex-row">
-                <div className=" w-3/5 md:space-y-2 space-y-4 flex flex-col">
-                    <label htmlFor="Username" className="text-xl">Username</label>
-                    <input type="text" name="Username" id="Username" placeholder="enter your username" required onChange={(e)=>{setUser({...user,Username:e.target.value})}} className=" h-14 rounded-md px-2 shadow" />
-                </div>
-                <div className="flex md:space-y-2 space-y-4 h-full w-2/5 flex-col">
-                    <label htmlFor="Profile" className="text-xl">Profile</label>
-                    <input type="file" ref={inputFileRef} name="profile" placeholder="choose your profile" className=" h-14 text-center p-1 hover:cursor-pointer rounded-md shadow" id="profile" />
-                </div>
+    <div
+      className="h-screen flex flex-col text-sm items-center"
+    >
+      {contextHolder}
+      <Card title="SignIn to continue">
+        <Form
+          action=""
+          onFinish={HandleSubmit}
+          className="w-full space-y-1 h-5/6 justify-center items-center flex flex-col"
+          encType="multipart/form-data"
+        >
+          <div className="flex justify-between w-full">
+            <div className="flex flex-col space-y-1">
+              <label htmlFor="Username">Username</label>
+              <Input
+                onChange={(e) => setUser({ ...user, Username: e.target.value })}
+                required
+              />
             </div>
-            <div className="h-20 flex md:space-y-2 space-y-4 w-full flex-col">
-                <label htmlFor="Email" className="text-xl">Email</label>
-                <input type="email" name="Email" id="Email" placeholder="example@gmail.com" required onChange={(e)=>{setUser({...user,Email:e.target.value})}} className=" h-14 px-2 shadow rounded-md" />
+            <div className="flex flex-col ml-4 space-y-1">
+              <label htmlFor="Profile">Profile</label>
+              <Upload
+                onChange={({ file, fileList }) => {
+                  setUser({ ...user, Profile: file });
+                }}
+                accept="image/*"
+              >
+                <Button icon={<UploadOutlined />}>Upload</Button>
+              </Upload>
             </div>
-            <div className="h-20 w-full md:space-y-2 space-y-4 flex flex-col">
-                <label htmlFor="Password" className="text-xl">Password</label>
-                <div className="flex w-full h-14 shadow rounded-md ">
-                    <input type={show?"text":"password"} id="Password" name="Password" required min={8} pattern="[a-zA-Z0-9;?,@]{8,15}" onChange={(e)=>{setUser({...user,Password:e.target.value})}} className=" pl-2 flex-1" />
-                    {show?(<button onClick={()=>setShow(!show)} className="w-1/12 h-full"><EyeOutlined/></button>): (<button onClick={()=>setShow(!show)}  className="w-1/12 h-full "><EyeInvisibleOutlined /></button>)}
-                </div>
-            </div>
-            {
-                user.Password!=""&&(!user.Password.match("[a-zA-Z0-9;?,@]{8,15}"))&&<p className="text-red-400 w-4/6 text-wrap">Password must contain at least 8 characters with a mixture of uppercase, lowercase, numbers</p>
-            }
-            <div className="h-20 w-full md:space-y-2 space-y-4 flex flex-col">
-                <label htmlFor="ConfirmPassword" className="text-xl">ConfirmPassword</label>
-                <div className="flex w-full h-14 shadow rounded-md ">
-                    <input type={showC?"text":"password"} id="ConfirmPassword" required onChange={(e)=>{setUser({...user,ConfirmPassword:e.target.value})}} className="flex-1 pl-2" />
-                    {showC?(<button onClick={()=>setShowC(!showC)} className="w-1/12 h-full"><EyeOutlined/></button>): (<button onClick={()=>setShowC(!showC)}  className="w-1/12 h-full"><EyeInvisibleOutlined /></button>)}
-                </div>
-            </div>
-            {
-                user.Password!=""&&user.ConfirmPassword!=""&&user.ConfirmPassword!=user.Password&&<span className="text-red-400">ConfirmPassword must be equal to Password</span>
-            }
-            <div className="w-full md:justify-between justify-around items-center flex h-20 md:flex-row flex-col">
-                {
-                    isSubmit?<Spin  className='md:text-center' indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />} />:<button className="btn-submit" type="submit">Submit</button>
-                }
-                <Link href="/login" className="text-blue-400  font-bold md:self-end md:h-12 self-start hover:underline">You have acount <strong>SignIn</strong></Link>
-            </div>
-        </form>
+          </div>
+          <div className="flex flex-col space-y-1 w-full">
+            <label htmlFor="Email">Email</label>
+            <Input
+              onChange={(e) => setUser({ ...user, Email: e.target.value })}
+              type="email"
+              required
+            />
+            {user.Email.length!==0 &&(!user.Email.match("[a-zA-Z]+[a-zA-Z0-9]*@[a-zA-Z]{3,5}[.a-zA-Z]"))&& (
+              <span className="text-red-400 text-[12px] text-justify w-full">
+                Please your Email must be a valid email address
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col space-y-1 w-full">
+            <label htmlFor="Password">Password</label>
+            <Input.Password
+              onChange={(e) => setUser({ ...user, Password: e.target.value })}
+              required
+            />
+            {user.Password.length!==0 && user.Password.length<=8 && (
+              <span className="text-red-400 text-[12px] text-justify w-full">
+                Please your Password must be contain least 8 characters 
+              </span>
+            )}
+          </div>
+          <div>
+            <label htmlFor="ConfirmPassword">ConfirmPassword</label>
+            <Input.Password
+              onChange={(e) =>
+                setUser({ ...user, ConfirmPassword: e.target.value })
+              }
+              required
+            />
+            {user.ConfirmPassword.length!==0 &&user.Password.length!==0&&user.ConfirmPassword!==user.Password && (
+              <span className="text-red-400  text-[12px] text-justify w-full">
+                Please Confirm Password must be equal to Password
+              </span>
+            )}
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+            className="w-full justify-between items-center flex h-12 md:flex-row flex-col"
+          >
+            {isSubmit ? (
+              <Spin
+                className="md:text-center"
+                indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />}
+              />
+            ) : (
+              <Button
+                className=" self-end"
+                type="primary"
+                htmlType="submit"
+              >
+                Submit
+              </Button>
+            )}
+            <Link
+              href="/login"
+              className="text-blue-400  font-bold self-end hover:underline"
+            >
+              You have acount <strong>SignIn</strong>
+            </Link>
+          </div>
+        </Form>
+      </Card>
     </div>
-  )
-} 
+  );
+}
